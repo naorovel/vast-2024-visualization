b6ee7c6189abdbfca9d6aee2925fec8d936e7e7a
Letian <xuletians10101@me.com>
2025-03-26 00:24:54 -0600

    first time update

---
+++ src/pages/task3.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Visualization</h1>
+      <client-only>
+        <Task3 />
+      </client-only>
+    </div>
+  </template>
+  
+  <script setup>
+  import Task3 from '~/components/Task3.vue'
+  </script>
